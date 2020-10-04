b46e57b7ca857ba5a1f5731e3832a010b471b291
Denis <kirevden@gmail.com>
2020-10-04 22:37:09 +0300

    feat: '1-new-feature-search gitignore fix'

--- application/src/components/Search/Search.js
+++ application/src/components/Search/Search.js
@@ -32,16 +32,7 @@ render() {
 
   return (
     <div className={classes.root}>
-    <AppBar position="static">
-    <Tabs variant='fullWidth' value={value} onChange={this.handleChange}>
-    <Tab label="Servers" icon={<CameraIcon />} />
-  <Tab label="Projects" icon={<MovieCreationIcon />} />
-  </Tabs>
-  </AppBar>
-  <SwipeableViews axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'} index={value} onChangeIndex={this.handleChangeIndex} >
-    <TabContainer dir={theme.direction}><Movies /></TabContainer>
-    <TabContainer dir={theme.direction}><Directors /></TabContainer>
-    </SwipeableViews>
+      Search
     </div>
 );
 }
