e98d784acafb72e5ae12dbeae936e2a0e16a24e1
Denis <kirevden@gmail.com>
2020-10-04 22:48:21 +0300

    feat: '2-create-squash commits'

---
+++ application/src/components/Search/Search.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+class SimpleSearch extends React.Component {
+  state = {
+    value: 0,
+  };
+
+  handleChange = (event, value) => { this.setState({ value }); };
+
+render() {
+  const { classes} = this.props;
+
+  return (
+    <div className={classes.root}>
+      Search for branch   11111
+    </div>
+);
+}
+}
+
+export default withHocs(SimpleSearch);
